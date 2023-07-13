51125a6d936dfe2c22a16ac03522177039a4b6b0
LegendMind1 <spaceisnotnull@gmail.com>
2023-07-13 21:10:15 +0500

    Fourth Commit Added PrimeReact Mainmenu Styles

--- app/components/ui/MainBar.jsx
+++ app/components/ui/MainBar.jsx
@@ -2,6 +2,21 @@
 import Image from 'next/image'
 import React from 'react'
 
+
+//--------For Main Bar Prime ------------
+import { PrimeReactProvider } from 'primereact/api';
+import { Menubar } from 'primereact/menubar';
+import {InputText} from 'primereact/inputtext'
+
+//Icon Library
+import 'primeicons/primeicons.css';
+//theme
+import "primereact/resources/themes/noman/theme.css";    
+//core
+import "primereact/resources/primereact.min.css";  
+import { Blaka } from 'next/font/google';
+
+
 export default function MainBar() {
   return (
     
@@ -53,4 +68,171 @@ export default function MainBar() {
     </div> 
 
   )
+}
+
+
+export function MainBarPrime (){
+
+    const items = [
+        {
+            label: 'File',
+            icon: 'pi pi-fw pi-file',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-plus',
+                    items: [
+                        {
+                            label: 'Bookmark',
+                            icon: 'pi pi-fw pi-bookmark'
+                        },
+                        {
+                            label: 'Video',
+                            icon: 'pi pi-fw pi-video'
+                        },
+
+                    ]
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-trash'
+                },
+                {
+                    separator: true
+                },
+                {
+                    label: 'Export',
+                    icon: 'pi pi-fw pi-external-link'
+                }
+            ]
+        },
+        {
+            label: 'Edit',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+                {
+                    label: 'Left',
+                    icon: 'pi pi-fw pi-align-left'
+                },
+                {
+                    label: 'Right',
+                    icon: 'pi pi-fw pi-align-right'
+                },
+                {
+                    label: 'Center',
+                    icon: 'pi pi-fw pi-align-center'
+                },
+                {
+                    label: 'Justify',
+                    icon: 'pi pi-fw pi-align-justify'
+                },
+
+            ]
+        },
+        {
+            label: 'Users',
+            icon: 'pi pi-fw pi-user',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-user-plus',
+
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-user-minus',
+
+                },
+                {
+                    label: 'Search',
+                    icon: 'pi pi-fw pi-users',
+                    items: [
+                        {
+                            label: 'Filter',
+                            icon: 'pi pi-fw pi-filter',
+                            items: [
+                                {
+                                    label: 'Print',
+                                    icon: 'pi pi-fw pi-print'
+                                }
+                            ]
+                        },
+                        {
+                            icon: 'pi pi-fw pi-bars',
+                            label: 'List'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Events',
+            icon: 'pi pi-fw pi-calendar',
+            items: [
+                {
+                    label: 'Edit',
+                    icon: 'pi pi-fw pi-pencil',
+                    items: [
+                        {
+                            label: 'Save',
+                            icon: 'pi pi-fw pi-calendar-plus'
+                        },
+                        {
+                            label: 'Delete',
+                            icon: 'pi pi-fw pi-calendar-minus'
+                        }
+                    ]
+                },
+                {
+                    label: 'Archive',
+                    icon: 'pi pi-fw pi-calendar-times',
+                    items: [
+                        {
+                            label: 'Remove',
+                            icon: 'pi pi-fw pi-calendar-minus'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Quit',
+            icon: 'pi pi-fw pi-power-off'
+        }
+    ];
+
+        const start = 
+            <>
+             
+              <div className='px-8 pt-2'>
+                    
+                    <Image 
+                        src={`/images/library.png`} 
+                        width={80} 
+                        height={80}
+                        className='flex rounded-full p-2 items-center transition-opacity opacity-0 duration-[2s] shadow-slate-900 shadow-lg border-2 border-white'
+                        onLoadingComplete={(image) => image.classList.remove('opacity-0')} 
+
+                    />
+                </div>
+            </>
+        const end = 
+                <>
+                    <div className='w-full ml-48'>
+                        <div>
+                            <InputText placeholder="Search" type="text" className='w-[300px]' />
+                        </div>
+                        <div className='flex justify-around w-1/2 mt-2'>
+                            <button className='bg-slate-500 rounded-full text-red-800 text-lg font-RetroCool w-[80px] hover:bg-yellow-600'>Login</button>
+                            <button className='bg-yellow-500 rounded-full text-red-800 text-lg font-RetroCool w-[80px]'>Signup</button>
+                        </div>
+                    </div>
+                </>
+    return (
+        <PrimeReactProvider>
+            <div className="flex w-full shadow-sm shadow-[#222831] rounded-b-[70px] overflow-clip">
+                <Menubar model={items} start={start} end={end} />
+            </div>
+        </PrimeReactProvider>
+    )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,9 @@
 'use client'
 import { Inter } from 'next/font/google'
-import MainBar from './components/ui/MainBar'
+import MainBar, {MainBarPrime} from './components/ui/MainBar'
 import { Roboto, Akronim, Rubik_Glitch } from 'next/font/google'
-import { Menubar } from 'primereact/menubar';
-import {InputText} from 'primereact/inputtext'
 
-import { PrimeReactProvider } from 'primereact/api';
-
-import 'primeicons/primeicons.css';
-
-//theme
-import "primereact/resources/themes/noman/theme.css";    
-    
-//core
-import "primereact/resources/primereact.min.css";        
-
-
+   
 const roboto = Roboto({
   weight: '500',
   subsets: ['latin'],
@@ -36,138 +24,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-      const items = [
-        {
-            label: 'File',
-            icon: 'pi pi-fw pi-file',
-            items: [
-                {
-                    label: 'New',
-                    icon: 'pi pi-fw pi-plus',
-                    items: [
-                        {
-                            label: 'Bookmark',
-                            icon: 'pi pi-fw pi-bookmark'
-                        },
-                        {
-                            label: 'Video',
-                            icon: 'pi pi-fw pi-video'
-                        },
-
-                    ]
-                },
-                {
-                    label: 'Delete',
-                    icon: 'pi pi-fw pi-trash'
-                },
-                {
-                    separator: true
-                },
-                {
-                    label: 'Export',
-                    icon: 'pi pi-fw pi-external-link'
-                }
-            ]
-        },
-        {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-                {
-                    label: 'Left',
-                    icon: 'pi pi-fw pi-align-left'
-                },
-                {
-                    label: 'Right',
-                    icon: 'pi pi-fw pi-align-right'
-                },
-                {
-                    label: 'Center',
-                    icon: 'pi pi-fw pi-align-center'
-                },
-                {
-                    label: 'Justify',
-                    icon: 'pi pi-fw pi-align-justify'
-                },
-
-            ]
-        },
-        {
-            label: 'Users',
-            icon: 'pi pi-fw pi-user',
-            items: [
-                {
-                    label: 'New',
-                    icon: 'pi pi-fw pi-user-plus',
-
-                },
-                {
-                    label: 'Delete',
-                    icon: 'pi pi-fw pi-user-minus',
-
-                },
-                {
-                    label: 'Search',
-                    icon: 'pi pi-fw pi-users',
-                    items: [
-                        {
-                            label: 'Filter',
-                            icon: 'pi pi-fw pi-filter',
-                            items: [
-                                {
-                                    label: 'Print',
-                                    icon: 'pi pi-fw pi-print'
-                                }
-                            ]
-                        },
-                        {
-                            icon: 'pi pi-fw pi-bars',
-                            label: 'List'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'Events',
-            icon: 'pi pi-fw pi-calendar',
-            items: [
-                {
-                    label: 'Edit',
-                    icon: 'pi pi-fw pi-pencil',
-                    items: [
-                        {
-                            label: 'Save',
-                            icon: 'pi pi-fw pi-calendar-plus'
-                        },
-                        {
-                            label: 'Delete',
-                            icon: 'pi pi-fw pi-calendar-minus'
-                        }
-                    ]
-                },
-                {
-                    label: 'Archive',
-                    icon: 'pi pi-fw pi-calendar-times',
-                    items: [
-                        {
-                            label: 'Remove',
-                            icon: 'pi pi-fw pi-calendar-minus'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'Quit',
-            icon: 'pi pi-fw pi-power-off'
-        }
-    ];
-
-const start = <div className='w-[36px] h-[36px]'>
-              <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="40" className="mr-2"></img>
-            </div>
-const end = <InputText placeholder="Search" type="text" className="w-full" />;
+      
 
 
   return (
@@ -175,17 +32,13 @@ const end = <InputText placeholder="Search" type="text" className="w-full" />;
 
       <header className='flex w-full'>
         <div
-          className={`${rubik_glitch.className} text-2xl`}
+          className={`${rubik_glitch.className} text-2xl w-full`}
         >
-          <MainBar />
-          
+          {/* <MainBar /> */}
+          <MainBarPrime />
         </div>
       </header>
-      <PrimeReactProvider>
-        <div className="flex w-full">
-          <Menubar model={items} start={start} end={end} className='w-full' />
-        </div>
-      </PrimeReactProvider>
+     
       <div>
         <ul className='font-fasthand text-lg p-4 text-red-800'>
           <li>Added through Tailwind after importing in globals.css @import url('https://fonts.googleapis.com/css2?family=Fasthand&display=swap')</li>
